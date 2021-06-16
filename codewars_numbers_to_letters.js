b19071579013cde1f16471e914175fe9b5e505e9
Crew Spence <csspence123@gmail.com>
2021-06-16 06:56:46 -0500

    Refactored code.

--- codewars_numbers_to_letters.js
+++ codewars_numbers_to_letters.js
@@ -3,40 +3,10 @@ Given an array of numbers (in string format), you must return a string. The numb
 */
 
 const switcher = (x) => {
-  const key = {
-    '29' : ' ',
-    '28' : '?',
-    '27' : '!',
-    '26' : 'a',
-    '25' : 'b',
-    '24' : 'c',
-    '23' : 'd',
-    '22' : 'e',
-    '21' : 'f',
-    '20' : 'g',
-    '19' : 'h', 
-    '18' : 'i',
-    '17' : 'j',
-    '16' : 'k',
-    '15' : 'l',
-    '14' : 'm',
-    '13' : 'n',
-    '12' : 'o',
-    '11' : 'p',
-    '10' : 'q',
-    '9' : 'r',
-    '8' : 's',
-    '7' : 't',
-    '6' : 'u',
-    '5' : 'v',
-    '4' : 'w',
-    '3' : 'x',
-    '2' : 'y',
-    '1' : 'z'
-  }
+  const key = ' zyxwvutsrqponmlkjihgfedcba!? '
   let str = '';
   for(let i = 0; i < x.length; i++) {
-    str += key[x[i]];
+    str += key[Number(x[i])];
   }
 
   return str;
